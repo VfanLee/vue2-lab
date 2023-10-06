1e17904eacc46c9f1f15bf6341ebb76499960c16
VfanLee <fanfanfafafa@gmail.com>
2023-10-06 19:12:01 +0800

    v-model

---
+++ src/components/MyInput/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-show="visible" style="padding: 10px; border: 1px dashed">
+    <input type="text" :value="value" @input="e => $emit('input', e.target.value)" />
+    <button @click="() => $emit('update:visible', false)">close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'value', // 绑定的 prop 名称，默认为 “value”
+    event: 'input' // 触发更新的事件名称，默认为 “input”
+  },
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
---
+++ src/views/input-view.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <MyInput v-model="textValue" :visible.sync="isShow" />
+
+    <div>
+      <button @click="isShow = !isShow">click me</button>
+      {{ textValue }}
+    </div>
+  </div>
+</template>
+
+<script>
+import MyInput from '@/components/MyInput/index.vue'
+
+export default {
+  name: 'InputView',
+  data() {
+    return {
+      textValue: 'hello',
+      isShow: true
+    }
+  },
+  components: {
+    MyInput
+  }
+}
+</script>
